c48f85c0cd583039c031579234d3be278290c574
Zeke Sy <68287413+Zekesy@users.noreply.github.com>
2022-07-12 19:16:25 +0900

    Add files via upload
    HoT: initial revision

---
+++ HTgame.py
@@ -0,0 +1,19 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+import random
+h = 0 
+t = 0
+
+
+for x in range(3):
+    print('Tossing a coin...')
+    y = random.randint(1,2)
+    print('Round ' ,x+1,':' )
+    if y == 1:
+        print('Heads')
+        h+=1
+    elif y == 2: 
+        print('Tails')
+        t+=1
+print('Heads:' , h , 'Tails:' , t)
